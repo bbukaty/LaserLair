b404820b38de8ff34b8010e5c4b68c9a267fb349
bbukaty <buck.bukaty@gmail.com>
2018-03-05 23:29:29 -0800

    simple rotation-based movement

--- Assets/Scripts/BlockRobot.cs
+++ Assets/Scripts/BlockRobot.cs
@@ -6,7 +6,7 @@ public class BlockRobot: MonoBehaviour {
 
 	private LevelManager levelManager;
 	private Vector3Int levelPos;
-	private Vector3Int orientation;
+	public Vector3Int orientation;
 	public Transform corpsePrefab;
 
 	public void initLevelPos(Vector3Int startPos) {
@@ -16,21 +16,26 @@ public class BlockRobot: MonoBehaviour {
     void Start() {
 		// find the level manager object in the scene to get level data from
 		levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+		orientation = new Vector3Int();
+        for (int i = 0; i < 3; i++) {
+            Debug.Assert(Mathf.Abs((float)(int)transform.right[i] - transform.right[i]) < 0.001, "Warning: Level contains improperly oriented cube!");
+            orientation[i] = (int)transform.right[i];
+        }
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Left")) {
-			movePlayer(new Vector3Int(-1, 0, 0));
+			applyInput(new Vector3Int(-1, 0, 0));
 		}
 		if (Input.GetButtonDown ("Right")) {
-			movePlayer(new Vector3Int(1, 0, 0));
+			applyInput(new Vector3Int(1, 0, 0));
 		}
 		if (Input.GetButtonDown ("Up")) {
-			movePlayer(new Vector3Int(0, 0, 1));
+			applyInput(new Vector3Int(0, 0, 1));
 		}
 		if (Input.GetButtonDown ("Down")) {
-			movePlayer(new Vector3Int(0, 0, -1));
+			applyInput(new Vector3Int(0, 0, -1));
 		}
 	}
 
@@ -40,15 +45,16 @@ public class BlockRobot: MonoBehaviour {
 	}
 
 	void moveModel(Vector3Int movement) {
-		transform.Translate(movement);
+		transform.Translate(movement, Space.World);
 	}
 
 	bool applyInput(Vector3Int movement) {
 		if (modelIsMoving()) {
 			return false;
 		}
-		// if movement direction doesn't equal orientation
-		if (movement != levelPos) {
+		// if movement axis doesn't align with current orientation
+		if (movement != orientation && movement != orientation * -1) {
+			Debug.Log("rotating");
 			return rotatePlayer(movement);
 		}
 		return movePlayer(movement);
@@ -61,7 +67,6 @@ public class BlockRobot: MonoBehaviour {
 			return false;
 		}
 		Block occupant = levelManager.getBlockIn(newPos);
-		//bool canPushOccupant = true; TODO: get this from occupant
 		if (occupant == null || levelManager.tryPush(newPos, movement)) {
 			/* maybe check if there's solid ground there first
 			int[] belowNewPos = (int[])newPos.Clone();
@@ -88,8 +93,9 @@ public class BlockRobot: MonoBehaviour {
 	}
 
 	private bool rotatePlayer(Vector3Int movement) {
-
+		transform.right = movement;
+		orientation = movement;
 		return true;
 	}
-	
+
 }
