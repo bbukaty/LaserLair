f996d57e441864d600fe1e96fc9270cd40695494
bbukaty <buck.bukaty@gmail.com>
2018-03-06 22:20:19 -0800

    basic robot spawn buttons

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour {
 
+	public Transform robotButton;
 	public Transform[] robotPrefabs;
 	public Vector3Int spawnLoc;
 	
-	// Update is called once per frame
-	void Update () {
-		//TODO: switch between robot prefabs in robotPrefabs array
-		//have icons for each type of robot that you click on to change selected robot
-		if (Input.GetButtonDown("Jump")) {
-			Robot newRobot = Instantiate(robotPrefabs[0], spawnLoc, Quaternion.identity).GetComponent<Robot>();
-			newRobot.rotateModelTo(new Vector3Int(0,0,1));
-			newRobot.setLevelPos(spawnLoc);
+	void Start() {
+
+		foreach (Transform robotPrefab in robotPrefabs) {
+			Transform buttonTransform = Instantiate(robotButton, transform);
+			buttonTransform.GetComponent<Button>().onClick.AddListener(delegate{spawn(robotPrefab);});
 		}
 	}
 
+	public void spawn(Transform robotPrefab) {
+		Robot newRobot = Instantiate(robotPrefab, spawnLoc, Quaternion.identity).GetComponent<Robot>();
+		newRobot.setLevelPos(spawnLoc);
+	}
 
 }
